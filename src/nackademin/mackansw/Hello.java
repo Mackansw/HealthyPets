14f846b1b2f43c4aecf0e10632c048f7a89e2c53
Marcus <mackansw@gmail.com>
2019-09-27 11:01:08 +0200

    hello world

---
+++ src/nackademin/mackansw/Hello.java
@@ -0,0 +1,11 @@
+package nackademin.mackansw;
+
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello " + System.getProperty("user.name") + "!");
+    }
+}
